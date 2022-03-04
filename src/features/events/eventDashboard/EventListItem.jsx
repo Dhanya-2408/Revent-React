fc2fa1c77c1992cf4db3026f76e33497b3a62362
Dhanya <dhanyaanu888@gmail.com>
2022-03-04 10:08:39 +0530

    Router Update

--- src/features/events/eventDashboard/EventListItem.jsx
+++ src/features/events/eventDashboard/EventListItem.jsx
@@ -1,6 +1,7 @@
-import React from "react";
-import { Segment, Item, Icon, List, Button } from "semantic-ui-react";
-import EventListAttendee from "./EventListAttendee";
+import React from 'react';
+import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
+import EventListAttendee from './EventListAttendee';
+import { Link } from 'react-router-dom';
 
 export default function EventListItem({ event, selectEvent, deleteEvent }) {
   return (
@@ -8,7 +9,7 @@ export default function EventListItem({ event, selectEvent, deleteEvent }) {
       <Segment>
         <Item.Group>
           <Item>
-            <Item.Image size="tiny" circular src={event.hostPhotoURL} />
+            <Item.Image size='tiny' circular src={event.hostPhotoURL} />
             <Item.Content>
               <Item.Header content={event.title} />
               <Item.Description>Hosted by {event.hostedBy}</Item.Description>
@@ -18,8 +19,8 @@ export default function EventListItem({ event, selectEvent, deleteEvent }) {
       </Segment>
       <Segment>
         <span>
-          <Icon name="clock" /> {event.date}
-          <Icon name="marker" /> {event.venue}
+          <Icon name='clock' /> {event.date}
+          <Icon name='marker' /> {event.venue}
         </span>
       </Segment>
       <Segment secondary>
@@ -33,15 +34,15 @@ export default function EventListItem({ event, selectEvent, deleteEvent }) {
         <div>{event.description}</div>
         <Button
           onClick={() => deleteEvent(event.id)}
-          color="red"
-          floated="right"
-          content="Delete"
+          color='red'
+          floated='right'
+          content='Delete'
         />
         <Button
-          onClick={() => selectEvent(event)}
-          color="teal"
-          floated="right"
-          content="View"
+          as={Link} to={`/events/${event.id}`}
+          color='teal'
+          floated='right'
+          content='View'
         />
       </Segment>
     </Segment.Group>
--- src/features/events/eventForm/EventForm.jsx
+++ src/features/events/eventForm/EventForm.jsx
@@ -1,14 +1,15 @@
 import React, { useState } from 'react';
 import { Segment, Header, Form, Button } from 'semantic-ui-react';
 import cuid from 'cuid';
+import { Link } from 'react-router-dom';
 
 export default function EventForm({
   setFormOpen,
+  setEvents,
   createEvent,
   selectedEvent,
   updateEvent,
 }) {
-  
   const initialValues = selectedEvent ?? {
     title: '',
     category: '',
@@ -98,7 +99,7 @@ export default function EventForm({
         </Form.Field>
         <Button type='submit' floated='right' positive content='Submit' />
         <Button
-          onClick={() => setFormOpen(false)}
+          as={Link} to='/events'
           type='submit'
           floated='right'
           content='Cancel'
